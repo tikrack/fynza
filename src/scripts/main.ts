92744a473cb681e2a647021698a81a85d733e0a7
Tikrack <tikrackcode@gmail.com>
2026-06-29 13:01:55 +0330

    finish adding swiper

--- src/scripts/main.ts
+++ src/scripts/main.ts
@@ -1,1 +1,45 @@
-import '../styles/app.css'
+// @ts-ignore
+import "swiper/css";
+// @ts-ignore
+import "swiper/css/navigation";
+// @ts-ignore
+import "swiper/css/pagination";
+
+import "../styles/app.css";
+
+import Swiper from "swiper";
+import { Navigation, Pagination } from "swiper/modules";
+
+new Swiper(".swiper", {
+  modules: [Navigation, Pagination],
+
+  slidesPerView: 1,
+  spaceBetween: 24,
+
+  breakpoints: {
+    768: {
+      slidesPerView: 2,
+      spaceBetween: 24,
+    },
+    1200: {
+      slidesPerView: 3,
+      spaceBetween: 32,
+    },
+  },
+
+  pagination: {
+    el: ".swiper-pagination",
+    clickable: true,
+  },
+
+  navigation: {
+    nextEl: ".swiper-button-next",
+    addIcons: false,
+    prevEl: ".swiper-button-prev",
+  },
+
+  scrollbar: {
+    el: ".swiper-scrollbar",
+    draggable: true,
+  },
+});
